ffe715839420c11156ad5dfbd73c6342c4b1d2c4
pixelverseio <support.pixelss@gmail.com>
2026-05-08 19:07:30 +0300

    Abdulla - global updated

--- js/global.js
+++ js/global.js
@@ -1,54 +1,58 @@
-if(localStorage.getItem("pixeluser")){
-    var user = localStorage.getItem("pixeluser");
-}
-else{
-    var user = {
-        username: "no User"
-    };
+if (localStorage.getItem("pixeluser")) {
+  var user = localStorage.getItem("pixeluser");
+} else {
+  var user = {
+    username: "no User",
+  };
 }
 
 // dark-mood nav toggle (small screens)
 let navToggle = document.querySelector(".nav-toggle");
 let navMenu = document.querySelector(".nav-menu");
 
-let sign_in_up = document.querySelector(".sign-in-up")
-if(!sign_in_up){
-    navToggle.addEventListener("click", function(){
-        navToggle.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    })
+let sign_in_up = document.querySelector(".sign-in-up");
+if (!sign_in_up) {
+  navToggle.addEventListener("click", function () {
+    navToggle.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
 }
 
 // light-mood nav toggle
 let ltNavToggle = document.querySelector(".lightnav-toggle");
 let header = document.querySelector("header");
 
-if(!sign_in_up){
-ltNavToggle.addEventListener("click", function(){
+if (!sign_in_up) {
+  ltNavToggle.addEventListener("click", function () {
     ltNavToggle.classList.toggle("active-light");
     header.classList.toggle("active-light");
-})
+  });
 }
-
 
 // Login Nav
 let signinBtn = document.querySelector(".sign-in");
 let signinUserContainer = document.querySelector("#signin-userContainer");
 let logo = document.querySelector(".nav-logo");
 let currentTheme = localStorage.getItem("theme");
-let login = localStorage.getItem("login")
+let login = localStorage.getItem("login");
 
-// User account icon
+// User & Admin account icon
 let profile = document.createElement("div");
-profile.innerHTML = `<i class = "fa-solid fa-circle-user"></i>`
+if (login == "admin") {
+  profile.innerHTML = `<i class="fa-solid fa-circle-plus"></i>`;
+  let add_game = profile.addEventListener("click",function(){
+    window.location.href = "add_game.html";
+  });
+} else {
+  profile.innerHTML = `<i class = "fa-solid fa-circle-user"></i>`;
+}
 profile.classList.add("user-photo");
 
 // Drop down menu
 let subMenuContainer = document.createElement("div");
-subMenuContainer.classList.add("menu-container")
+subMenuContainer.classList.add("menu-container");
 let balance = 0;
-subMenuContainer.innerHTML =
-`<ul class="sub-menu">
+subMenuContainer.innerHTML = `<ul class="sub-menu">
     <li class="user-info">
         <i class="fa-solid fa-circle-user"></i>
         <p class="username">${user}</p>
@@ -84,97 +88,95 @@ subMenuContainer.innerHTML =
 // when user icon clicked => User Info appear & support hidden from nav
 let sessionDepLi = document.querySelector(".session-dependentLink");
 let dynamicLink = sessionDepLi.firstElementChild;
-if(login == "user"){
-    // add drop menu
-    signinUserContainer.append(subMenuContainer);
-    // add username to profile
-    let usernameP = document.createElement("p");
-    usernameP.innerHTML = `${user}`;
-    profile.append(usernameP);
-    // remove support link (added to drop menu)
-    dynamicLink.setAttribute("href", "library.html");
-    dynamicLink.firstElementChild.className = "";
-    dynamicLink.firstElementChild.classList.add("bi", "bi-grid-3x3-gap-fill");
-    // dynamicLink.firstElementChild.classList.add("fa-solid", "fa-layer-group");
+if (login == "user") {
+  // add drop menu
+  signinUserContainer.append(subMenuContainer);
+  // add username to profile
+  let usernameP = document.createElement("p");
+  usernameP.innerHTML = `${user}`;
+  profile.append(usernameP);
+  // remove support link (added to drop menu)
+  dynamicLink.setAttribute("href", "library.html");
+  dynamicLink.firstElementChild.className = "";
+  dynamicLink.firstElementChild.classList.add("bi", "bi-grid-3x3-gap-fill");
+  // dynamicLink.firstElementChild.classList.add("fa-solid", "fa-layer-group");
 
-    dynamicLink.lastElementChild.innerHTML = "Library";
+  dynamicLink.lastElementChild.innerHTML = "Library";
 
-    if(currentTheme == "dark"){
-        signinUserContainer.append(profile);
-    }
-    else if(currentTheme == "light"){
-        logo.after(profile);
-    }
-    signinBtn.remove();
+  if (currentTheme == "dark") {
+    signinUserContainer.append(profile);
+  } else if (currentTheme == "light") {
+    logo.after(profile);
+  }
+  signinBtn.remove();
+} else if (login == "admin") {
+  if (currentTheme == "dark") {
+    signinUserContainer.append(profile);
+  } else if (currentTheme == "light") {
+    logo.after(profile);
+  }
+  signinBtn.remove();
 }
-profile.addEventListener("click",function(){
-    subMenuContainer.classList.toggle("active");
-})
+profile.addEventListener("click", function () {
+  subMenuContainer.classList.toggle("active");
+});
 //logout
 logout = document.querySelector(".logout");
 // check if the logout button exists (user logged in)
-if(logout){
-    logout.addEventListener("click", function(){
-        localStorage.setItem("login", "");
-    })
+if (logout) {
+  logout.addEventListener("click", function () {
+    localStorage.setItem("login", "");
+  });
 }
 
 // Dark/Light theme switching
-window.addEventListener("load", function(){
-    let themeToggle = document.querySelector("label.theme-toggle");
-let link = document.querySelector("link.theme");
-if(currentTheme == "light"){
+window.addEventListener("load", function () {
+  let themeToggle = document.querySelector("label.theme-toggle");
+  let link = document.querySelector("link.theme");
+  if (currentTheme == "light") {
     link.setAttribute("href", "css/light-mood.css");
     link.classList.remove("dark-theme");
     link.classList.add("light-theme");
     document.documentElement.className = "light-theme";
     // document.classList.add("");
-}else{
+  } else {
     link.setAttribute("href", "css/global.css");
     link.classList.remove("light-theme");
     link.classList.add("dark-theme");
     document.documentElement.className = "dark-theme";
     // document.classList.add("dark-theme");
-}
-themeToggle.addEventListener("click", function(){
-    if(link.classList.contains("dark-theme")){
-        // change theme to light
-        link.setAttribute("href", "css/light-mood.css");
-        link.classList.remove("dark-theme");
-        link.classList.add("light-theme");
-        localStorage.setItem("theme", "light");
-        document.documentElement.className = "light-theme";
-        // change profile location
-        if(login == "user"){
+  }
+  themeToggle.addEventListener("click", function () {
+    if (link.classList.contains("dark-theme")) {
+      // change theme to light
+      link.setAttribute("href", "css/light-mood.css");
+      link.classList.remove("dark-theme");
+      link.classList.add("light-theme");
+      localStorage.setItem("theme", "light");
+      document.documentElement.className = "light-theme";
+      // change profile location
+      if (login == "user" || login == "admin") {
         profile.remove();
         profile.style.color = "#252444";
         logo.after(profile);
-        }
-    }
-    else{
-        // change theme to dark
-        link.setAttribute("href", "css/global.css");
-        link.classList.remove("light-theme");
-        link.classList.add("dark-theme");
-        localStorage.setItem("theme", "dark");
-        document.documentElement.className = "dark-theme";
-        // change profile location
-        if(login == "user"){
+      }
+    } else {
+      // change theme to dark
+      link.setAttribute("href", "css/global.css");
+      link.classList.remove("light-theme");
+      link.classList.add("dark-theme");
+      localStorage.setItem("theme", "dark");
+      document.documentElement.className = "dark-theme";
+      // change profile location
+      if (login == "user" || login == "admin") {
         profile.remove();
         profile.style.color = "#c7c7c7";
         signinUserContainer.append(profile);
-        }
+      }
     }
-})
-})
-
+  });
+});
 
 // localStorage.setItem("pixelUser", JSON.stringify(userobj));
 
 // let obj = JSON.parse(localStorage.getItem("pixelUser"));
-
-
-
-
-
-
