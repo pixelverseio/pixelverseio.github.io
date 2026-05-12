2b367922fc97b8d9e415d88270ce5b03b672d73b
pixelverseio <support.pixelss@gmail.com>
2026-05-12 03:27:27 +0300

    Marwan - library.js added comments by mo

--- js/library.js
+++ js/library.js
@@ -1,19 +1,19 @@
-// console.log("gamesData = ",gamesData);
-
-// console.log("users = ",users);
-
+// get the username from local storage 
 let username = localStorage.getItem("pixeluser");
 console.log("username = ",username);
-
+//select the container where game cards will be displayed
 const container = document.getElementById("library-container");
 console.log("username = ",username)
+//loop over the user games library
 for(let i = 0 ; i<users[username].library.length ; i++){
     let game_key = users[username].library[i]
     let gameOb = gamesData[users[username].library[i]];
     console.log("gameOb = ",gameOb)
+    //create a new div element for the game card
     const card = document.createElement("div");
     card.className = "game-card";
     card.id = game_key;
+    //write the card with HTML content image and title and button
     card.innerHTML = `
         <span class="badge">${gameOb.badge}</span>
         <img src="${gameOb.image}">
@@ -22,23 +22,29 @@ for(let i = 0 ; i<users[username].library.length ; i++){
             <span>🎮</span> Play
         </button>
         `;
+        //add the card to the container in the webpage
         container.appendChild(card);
-
+        //redirect to the game page when the card is clicked
         card.addEventListener("click", function(){
         window.location.href = `${game_key}.html`;
         });
     }
-
-const searchBox = document.querySelector('.search-box');
-searchBox.addEventListener('input', (e) => {
+//Select the class where search box will be displayed
+const searchBox = document.querySelector(".search-box");
+// Run this function every time the input value changes
+searchBox.addEventListener("input", (e) => {
+    //Make all the inputs small
     let word = e.target.value.toLowerCase();
-    let cards = document.querySelectorAll('.game-card'); 
+    //Select all elements with the class game-card and store them in a list
+    let cards = document.querySelectorAll(".game-card"); 
+    //check each card to see if it matches the search word
     cards.forEach(card => {
-        let title = card.querySelector('h3').textContent.toLowerCase();
+         //made the game name Small and we are searching for it
+        let title = card.querySelector("h3").textContent.toLowerCase();
         if (title.includes(word)) {
-            card.style.display = "block";
+            card.style.display = "block";// Show card
         } else {
-            card.style.display = "none";
+            card.style.display = "none";//hide card
         }
     });
 });
