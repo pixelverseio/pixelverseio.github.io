ca839918894cfa0f8e632b4d6d28ae24b8ab3d97
pixelverseio <support.pixelss@gmail.com>
2026-06-21 16:03:20 +0300

    Abdulla - balance JS updated

--- js/balance.js
+++ js/balance.js
@@ -3,16 +3,34 @@ let form = document.forms[0];
 let numInput = document.querySelector("[name='num']");
 let balInput = document.querySelector("[name='bal']");
 
-// Success Message
-let chargeMsg = document.createElement("div");
-chargeMsg.className = "chargeMsg";
-chargeMsg.innerHTML = `
-    <p>Charged Successfully</p>
+let loading = document.createElement("div");
+loading.innerHTML = `
+  <div class="loading">
+    <p>Loading</p>
+    <div class="circle"></div>
+  </div>
+`;
+balInput.after(loading);
+loading.style.display = "none";
+
+let loaded = document.createElement("div");
+loaded.className = "successlogin";
+loaded.id = "successlogin";
+loaded.innerHTML = `
+    <p>Loaded Successfully</p>
     <i class="fa-solid fa-circle-check" style="color: rgb(99, 230, 190)"></i>
 `;
-document.body.appendChild(chargeMsg);
+balInput.after(loaded);
+loaded.style.display = "none";
 
-// Error Message
+let mustfill = document.createElement("p");
+mustfill.className = "errChargeMsg";
+mustfill.innerHTML = `
+  <i class="fa-solid fa-triangle-exclamation"></i> All Fields should be filled!
+`;
+form.insertBefore(mustfill, form.children[1]);
+mustfill.style.display = "none";
+
 let errChargeMsg = document.createElement("div");
 errChargeMsg.className = "errChargeMsg";
 errChargeMsg.innerHTML = `
@@ -25,12 +43,12 @@ errChargeMsg.innerHTML = `
 form.insertBefore(errChargeMsg, form.children[1]);
 
 errChargeMsg.style.display = "none";
-chargeMsg.style.display = "none";
+
 form.onsubmit = function (event) {
   event.preventDefault();
 
   let walletNum = numInput.value.trim();
-  let balanceInput = Number(balInput.value.trim());
+  let balanceInput = balInput.value.trim();
 
   let numValid = false;
   let balValid = false;
@@ -44,22 +62,35 @@ form.onsubmit = function (event) {
   if (balanceInput !== "" && Number(balanceInput) > 0) {
     balValid = true;
   }
+  if (balanceInput === "" || walletNum === "") {
+    mustfill.style.display = "block";
+    setTimeout(() => {
+      mustfill.style.display = "none";
+    }, 2000);
+    return;
+  }
+    if (!numValid || !balValid) {
+      errChargeMsg.style.display = "block";
+      setTimeout(function () {
+        errChargeMsg.style.display = "none";
+      }, 3000);
+      return;
+    }
+  else {
+    loading.style.display = "block";
 
-  if (!numValid || !balValid) {
-    errChargeMsg.style.display = "block";
-    setTimeout(function () {
-      errChargeMsg.style.display = "none";
-    }, 3000);
-  } else {
     // console.log(balance);
-    userData.balance += balanceInput;
+    userData.balance += Number(balanceInput);
     // console.log(balance);
     // console.log(users);
     localStorage.setItem("users", JSON.stringify(users));
-    chargeMsg.style.display = "block";
-    form.style.display = "none";
-    setTimeout(function () {
-      window.location.href = "balance.html";
+    setTimeout(() => {
+      loading.style.display = "none";
+      loaded.style.display = "flex";
     }, 3000);
+
+    setTimeout(() => {
+      window.location.href = "balance.html";
+    }, 4500);
   }
 };
